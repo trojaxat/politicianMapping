872c40c8fe9f58968655d915cd8814ccd3596617
trojaxat <dan.axford89@gmail.com>
2020-07-21 18:31:54 +0200

    different way to start constructor in typesceript

--- features/Politician.ts
+++ features/Politician.ts
@@ -1,21 +1,9 @@
 export default class Politician {
-  id: number;
-  name: string;
-  age: number;
-  decision: boolean;
-  party: string;
-
   constructor(
-    id: number,
-    name: string,
-    age: number,
-    decision: boolean,
-    party: string
-  ) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.decision = decision;
-    this.party = party;
-  }
+    public id: number,
+    public name: string,
+    public age: number,
+    public decision: boolean,
+    public party: string
+  ) {}
 }
